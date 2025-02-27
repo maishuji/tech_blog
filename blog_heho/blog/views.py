e71046ed1bbb6252cf7b3250f1bb3c11ec7d6b87
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-02-27 22:14:53 +0100

    Process markdown to html for blog content

--- blog_heho/blog/views.py
+++ blog_heho/blog/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
+import markdown
 
 def blog_list(request):
-    posts = BlogPost.objects.all()  # Get all blog posts from the database
+    posts = BlogPost.objects.all()
+
+    # Convert each post's content from Markdown to HTML
+    for post in posts:
+        post.content = markdown.markdown(post.content, extensions=["extra", "fenced_code", "toc"])
+
     return render(request, 'blog/blog_list.html', {'posts': posts})
+
+    #posts = BlogPost.objects.all()  # Get all blog posts from the database
+    #return render(request, 'blog/blog_list.html', {'posts': posts})
 
 def blog_detail_view(request, post_id):
     post = get_object_or_404(BlogPost, id=post_id)
+    post.content = markdown.markdown(post.content, extensions=["extra", "fenced_code", "toc"])
     return render(request, "blog/blog_detail.html", {"post": post})
 
 def contact_view(request):
+    
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
@@ -26,4 +37,8 @@ def contact_view(request):
 
 def home_view(request):
     posts = BlogPost.objects.order_by('-id')[:5]  # Fetch the latest 5 blog posts
+
+    # Convert each post's content from Markdown to HTML
+    for post in posts:
+        post.content = markdown.markdown(post.content, extensions=["extra", "fenced_code", "toc"])
     return render(request, "home.html", {"posts": posts})
