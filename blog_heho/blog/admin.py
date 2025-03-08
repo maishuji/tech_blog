02819ab31a13fd6a4dd83255523131953538c238
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-03-08 09:59:59 +0100

    Add categories

--- blog_heho/blog/admin.py
+++ blog_heho/blog/admin.py
@@ -2,7 +2,8 @@
 Admin page for the blog app
 '''
 from django.contrib import admin
-from .models import BlogPost, BlogImage
+from .models import BlogPost, BlogImage, Tag
 
 admin.site.register(BlogPost)
+admin.site.register(Tag)
 admin.site.register(BlogImage)
--- blog_heho/blog/models.py
+++ blog_heho/blog/models.py
@@ -3,6 +3,18 @@ Module containing the models for the blog
 '''
 from django.db import models
 
+class Tag(models.Model):
+    '''
+    Model to store blog tags
+    '''
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        '''
+        String representation of the model
+        '''
+        return str(self.name)
+
 class BlogPost(models.Model):
     '''
     Model to store blog posts
@@ -10,6 +22,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
     objects = models.Manager()  # Explicitly define objects manager (not necessary, but for pylint)
     def __str__(self):
         '''
