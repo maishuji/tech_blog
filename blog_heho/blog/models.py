50b5bcf4cd62a094f29e46489c4494254407b713
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-04-26 10:50:48 +0200

    Add filter for article categories

--- blog_heho/blog/models.py
+++ blog_heho/blog/models.py
@@ -2,12 +2,19 @@
 Module containing the models for the blog
 '''
 from django.db import models
+from django.utils.text import slugify
 
 class Tag(models.Model):
     '''
     Model to store blog tags
     '''
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=120, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         '''
